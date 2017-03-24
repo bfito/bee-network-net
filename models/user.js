360b98c79ef2cabf1d8d69e967015208acc47fed
JPZ <jeanpaulzune@gmail.com>
2017-03-24 09:19:33 -0400

    first commit of bee-network-net as seperate repo

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    required: [true, 'Username required']
+  },
+  password: {
+    type: String,
+    required: [true, 'Password required']
+  },
+  name: {
+    type: String,
+    required: [true, 'Name required']
+  },
+  address: {
+    street: String,
+    city: String,
+    state: String,
+    zipcode: Number,
+  },
+  role: {
+    farm: Boolean,
+    beekeeper: Boolean,
+  }
+
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
